157292fbeb298d605276f62017e8e1fd05e35b0a
mariagn95 <mariaagn95@gmail.com>
2020-05-01 19:54:23 +0200

    Generación de página dinámica con plantilla en el código

--- P2/mi_proyectoweb/mi_tienda/views.py
+++ P2/mi_proyectoweb/mi_tienda/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from random import randint
+from django.template import Template, Context
 
 # -- Vista principal de mi tienda
 # -- El nombre de la vista puede ser cualquiera. Nosotros lo hemos
@@ -39,3 +40,43 @@ def test1(request):
 
 # -- El mensaje de respuesta se contruye a partir del
 # -- código HTML empotrado en cadenas.
+
+
+
+
+# -- Ejemplo de generacion mediante una plantilla en el código
+def test2(request):
+
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+
+    PLANTILLA = """
+    <!DOCTYPE html>
+    <html lang="es" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Test2</title>
+      </head>
+      <body>
+        <h1>TEST2</h1>
+        <p> Numero aleatorio:  {{numero}} </p>
+      </body>
+    </html>
+    """
+
+    # --Procesar la plantilla
+    t = Template(PLANTILLA)
+
+    # -- Crear el contexto: Asignar el numero
+    c = Context({'numero': str(numero)})
+
+    # -- Obtener la pagina html final
+    html = t.render(c)
+
+    return HttpResponse(html)
+
+# -- La plantilla es HTML, que contiene un campo genérico {{numero}} donde
+# -- se colocará el número aleatorio. Esta asociación entre el número aleatorio
+# -- y la variable {{numero}} se realiza en el contexto. Luego se crea
+# -- la página HTML final a partir de este contexto y se envía al cliente
+# -- para su visualización
