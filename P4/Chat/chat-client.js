e830f5dd9842b6124013c940b9c08856bb8d980e
mariagn95 <mariaagn95@gmail.com>
2020-05-16 20:20:09 +0200

    Chat en proceso

--- P4/Chat/chat-client.js
+++ P4/Chat/chat-client.js
@@ -1,40 +1,33 @@
-console.log("Ejecutando cliente JS...");
+function main() {
+  var nick = prompt("¿Qué Nick quiere utilizar para este chat");
+  console.log("Ejecutando cliente JS...");
 
-//-- Obtener los elementos del DOM
-const display = document.getElementById("display");
-const msg = document.getElementById("msg");
-const send = document.getElementById("send");
+  //-- Obtener los elementos del DOM
+  const display = document.getElementById("display");
+  const msg = document.getElementById("msg");
+  const send = document.getElementById("send");
 
-//-- Crear un websocket. Se establece la conexión con el servidor
-const socket = io();
+  //-- Crear un websocket. Se establece la conexión con el servidor
+  const socket = io();
 
-//-- Se ha recibido el evento 'hello':
-//-- Es el mensaje de bienvenida del servidor
-socket.on('hello', (msg) => {
+  //-- Se envia al Usuario
+  socket.emit('new_client', nick);
 
-  //-- Mostrarlo en la consola del navegador, para
-  //-- depurar
-  console.log("Mensaje del servidor: " + msg);
+  //-- Botón de envío apretado
+  send.onclick = () => {
+    //-- Se envía el mensaje escrito
+    //-- Usamos el nombre 'msg' para los mensajes de usuario
+    //-- Si no se ha introducido ningún mensaje, no se envía
+    if (msg.value)
+      socket.emit('new_message', msg.value)
+      console.log("Mensaje enviado");
+    //-- Borramos el mensaje escrito
+    msg.value="";
+  }
 
-  //-- Ponerlo en el párrafo display
-  display.innerHTML = msg;
-});
-
-//-- Se ha recibido un mensaje
-socket.on('msg', (msg) => {
-  //-- Añadirlo al párrafo display
-  display.innerHTML += "<br> > " + msg;
-});
-
-//-- Botón de envío apretado
-send.onclick = () => {
-
-  //-- Se envía el mensaje escrito
-  //-- Usamos el nombre 'msg' para los mensajes de usuario
-  //-- Si no se ha introducido ningún mensaje, no se envía
-  if (msg.value)
-    socket.emit('msg', msg.value)
-
-  //-- Borramos el mensaje escrito
-  msg.value="";
+  //-- Se ha recibido un mensaje
+  socket.on('new_message', (msg) => {
+    //-- Añadirlo al párrafo display
+    display.innerHTML += "<br> > " + msg;
+  });
 }
--- P4/Chat/chat-server.js
+++ P4/Chat/chat-server.js
@@ -15,6 +15,9 @@ const io = require('socket.io')(http);
 //-- Puerto donde lanzar el servidor
 const PORT = 8080
 
+//-- Usuarios del Chat
+var users = 0;
+
 //-- Lanzar servidor
 http.listen(PORT, function(){
   console.log('Servidor lanzado en puerto ' + PORT);
@@ -23,16 +26,26 @@ http.listen(PORT, function(){
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Página principal
 app.get('/', (req, res) => {
-  let path = __dirname + '/chat.html';
+  let path = __dirname + '/index.html';
   res.sendFile(path);
-  console.log("Acceso a " + path);
+  console.log("Accediendo al index");
+  console.log("  ");
 });
 
-//-- Otra vista de prueba
-app.get('/woala', (req, res) => {
-  res.send('WOALA! Chuck Norris approved!! :-)');
-  console.log("Acceso a /woala");
+//-- Página del cliente
+app.get('/chat-client.js', (req,res)=>{
+  res.sendFile(__dirname + '/chat-client.js');
+  console.log("Fichero de JavaScrip solicitado ");
+  console.log("  ");
 });
+
+//-- Hoja de estilo
+app.get('/css/style.css', (req,res)=>{
+  res.sendFile(__dirname + '/css/style.css');
+  console.log("Fichero de css solicitado ");
+  console.log("  ");
+});
+
 
 //-- El resto de peticiones se interpretan como
 //-- ficheros estáticos
@@ -44,22 +57,29 @@ app.use('/', express.static(__dirname +'/'));
 io.on('connection', function(socket){
 
   //-- Usuario conectado. Imprimir el identificador de su socket
-  console.log('--> Usuario conectado!. Socket id: ' + socket.id);
+  console.log('--> Usuario conectado!');
+  msg_res = "--> Nuevo usuario conectado";
+  io.emit('new_message', msg_res);
 
-  //-- Le damos la bienvenida a través del evento 'hello'
-  //-- ESte evento lo hemos creado nosotros para nuestro chat
-  socket.emit('hello', "Bienvenido al Chat");
+  //-- Le damos la bienvenida
+  msg_welcom = "   ¡Bienvenido a Chating!   ";
+  socket.emit('new_message', msg_welcom);
 
-  //-- Función de retrollamada de mensaje recibido del cliente
-  socket.on('msg', (msg) => {
-    console.log("Cliente: " + socket.id + ': ' + msg);
+  //-- Nuevo cliente
+  socket.on('new_client', (nick) => {
+    console.log(nick + " está conectado");
+    users += 1;
+    //-- Mensaje del cliente
+    socket.on('new_message', (msg) =>{
+      console.log('Mensaje recibido');
+      io.emit('new_message', msg);
+    });
 
-    //-- Enviar el mensaje a TODOS los clientes que estén conectados
-    io.emit('msg', msg);
-  })
+  });
 
   //-- Usuario desconectado. Imprimir el identificador de su socket
   socket.on('disconnect', function(){
-    console.log('--> Usuario Desconectado. Socket id: ' + socket.id);
+    console.log("--> Usuario Desconectado.");
+    users -= 1;
   });
 });
